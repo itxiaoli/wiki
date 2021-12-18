48a2d54da1aed20c91c02541bc3f433006629dd0
itxiaoli <1369193982@qq.com>
2021-12-18 22:39:18 +0800

    配置axios拦截器打印请求日志和返回日志

--- web/src/main.ts
+++ web/src/main.ts
@@ -18,3 +18,21 @@ const icons: any = Icons;
 for (const i in icons) {
   app.component(i, icons[i]);
 }
+
+
+/**
+ * axios拦截器
+ */
+axios.interceptors.request.use(function (config) {
+  console.log('请求参数：', config);
+  return config;
+}, error => {
+  return Promise.reject(error);
+});
+axios.interceptors.response.use(function (response) {
+  console.log('返回结果：', response);
+  return response;
+}, error => {
+  console.log('返回错误：', error);
+  return Promise.reject(error);
+});
